2c74e7c33b348b2fd83d635ae94292411bed1112
zhsong <67272893+zh-song@users.noreply.github.com>
2021-12-31 11:55:07 +0800

    Add files via upload

---
+++ Opencv_C++/MFCAPP.h
@@ -0,0 +1,32 @@
+
+// MFCAPP.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCAPPApp: 
+// 有关此类的实现，请参阅 MFCAPP.cpp
+//
+
+class CMFCAPPApp : public CWinApp
+{
+public:
+	CMFCAPPApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCAPPApp theApp;
